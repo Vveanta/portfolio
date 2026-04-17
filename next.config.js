18195da7f72adcfc84c78a3247c37700453fae43
Vveanta <104909463+Vveanta@users.noreply.github.com>
2026-04-17 16:18:46 -0400

    update resume

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   images: {
+    // Avoid `/_next/image` on serverless: Next 12’s optimizer uses a Lambda handler
+    // shape that Node.js 24 runtimes reject (callback-style handlers).
+    unoptimized: true,
     domains: [
       'res.cloudinary.com',
       'avatars.githubusercontent.com',
